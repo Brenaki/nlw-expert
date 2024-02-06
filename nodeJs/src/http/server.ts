69a0ec5728bcdc6b74307e3a0ceef3e1d4e0334f
Brenaki <victor.legat.cerqueira@gmail.com>
2024-02-05 21:13:51 -0300

    [Fix] Create migrate Poll

--- nodeJs/src/http/server.ts
+++ nodeJs/src/http/server.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client"
 import z from "zod";
 
 const app = fastify()
+const prisma = new PrismaClient()
 
 app.post("/polls", async (request, reply) => {
   const createPollBody = z.object({
@@ -11,7 +12,7 @@ app.post("/polls", async (request, reply) => {
 
   const {title} = createPollBody.parse(request.body)
 
-  const poll = await PrismaClient.poll.create({
+  const poll = await prisma.poll.create({
     data: {
       title
     }
