948068cce465ff3a40c47b7f8cc3671099fdb9a6
Brenaki <victor.legat.cerqueira@gmail.com>
2024-02-10 20:40:33 -0300

    Update new-note-card.tsx

--- reactJs/src/components/new-note-card.tsx
+++ reactJs/src/components/new-note-card.tsx
@@ -74,8 +74,8 @@ export const NewNoteCard = ({ onNoteCreated }: NewNoteCardProps) => {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50"/>
-        <Dialog.Content className="fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-lg flex flex-col outline-none">
-          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100 md:rounded-tr-lg">
+        <Dialog.Content className="fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 flex flex-col outline-none">
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
             <X className="size-5" />
           </Dialog.Close>
           <form className="flex flex-1 flex-col">
@@ -96,7 +96,7 @@ export const NewNoteCard = ({ onNoteCreated }: NewNoteCardProps) => {
                   <button
                   type="button"
                   onClick={handleStopRecording}
-                  className="w-full flex items-center justify-center gap-2 bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none font-medium hover:text-slate-100 md:rounded-b-lg">
+                  className="w-full flex items-center justify-center gap-2 bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none font-medium hover:text-slate-100">
                     <div className="size-3 rounded-full bg-red-500 animate-pulse"/>
                     Gravando (clique p/ interromper)
                   </button>
@@ -104,7 +104,7 @@ export const NewNoteCard = ({ onNoteCreated }: NewNoteCardProps) => {
                   <button
                   type="button"
                   onClick={handleSaveNote}
-                  className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500 md:rounded-b-lg">
+                  className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500">
                     Salvar nota
                   </button>
                 )}
